1ebc7170f670c319bfa19f51202880a1527863f1
Scott Spencer <scottspencer003@yahoo.com>
2020-02-10 13:41:45 -0500

    Corrections

--- StockServiceDemo/src/main/java/com/scottspencer/app/BasicStockService.java
+++ StockServiceDemo/src/main/java/com/scottspencer/app/BasicStockService.java
@@ -3,11 +3,10 @@ package com.scottspencer.app;
 public class BasicStockService implements StockService {
 	
 	public BasicStockService() {
-		new StockQuote(780.08, "TSLA");
 	}
 	
 	public StockQuote getQuote(String tickerSymbol) {
-		return new StockQuote();
+		return new StockQuote(780.08, "TSLA");
 	}
 
 }
--- StockServiceDemo/src/test/java/com/scottspencer/app/StockServiceFactoryTest.java
+++ StockServiceDemo/src/test/java/com/scottspencer/app/StockServiceFactoryTest.java
@@ -8,10 +8,10 @@ public class StockServiceFactoryTest {
 
 	@Test
 	public void stockServiceFactoryTest() {
-		
+		double val = 780.08;
 		StockQuote compareQuote = new StockQuote(780.08, "TSLA");
-		BasicStockService test = (BasicStockService) StockServiceFactory.getStockService();
-		assertTrue(compareQuote.getValue() == test.);
+		
+		assertEquals(StockServiceFactory.getStockService().getQuote("TSLA").getValue(), compareQuote.getValue());
 	}
 
 }
