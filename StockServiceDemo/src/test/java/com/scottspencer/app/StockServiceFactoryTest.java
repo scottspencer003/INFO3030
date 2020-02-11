871c090aab02e7c0d89dcf30f50a382cea79fafb
Scott Spencer <scottspencer003@yahoo.com>
2020-02-11 15:35:11 -0500

    Added unit test

--- StockServiceDemo/src/test/java/com/scottspencer/app/StockServiceFactoryTest.java
+++ StockServiceDemo/src/test/java/com/scottspencer/app/StockServiceFactoryTest.java
@@ -1,15 +1,32 @@
 package com.scottspencer.app;
 
 import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
 
 import org.junit.Test;
 
 public class StockServiceFactoryTest {
 
+
 	@Test
-	public void stockServiceFactoryTest() {
-		StockQuote compareQuote = new StockQuote();
-		assertTrue(compareQuote.getValue() == 780.08);
+	public void stockServiceFactoryTestSym() {
+		StockService testStock = StockServiceFactory.getStockService();
+		assertEquals(testStock.getQuote("TSLA").getTickerSymbol(), "TSLA");
+		
+	}
+	
+	@Test
+	public void stockServiceFactoryTestVal() {
+		StockService testStock = StockServiceFactory.getStockService();
+		assertTrue(testStock.getQuote("TSLA").getValue() == 780.08);
+		
+	}
+	
+	@Test
+	public void InstanceOfTest () {
+		StockService testStock = StockServiceFactory.getStockService();
+		BasicStockService comparisonStock = new BasicStockService();
+		assertEquals(comparisonStock, testStock);
 	}
 
 }
